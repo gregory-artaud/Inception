645b33be6188d36992093ada49fa7512baad52c4
Grégory Artaud <gregory.rtd@gmail.com>
2022-02-13 19:26:31 +0100

    wip db root pwd

--- srcs/requirements/mariadb/config/init.sh
+++ srcs/requirements/mariadb/config/init.sh
@@ -1,7 +1,11 @@
 service mysql start
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 	mysql_install_db
-
+	/usr/bin/mysqld_safe &
+	sleep 10s
+	echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" | mysql
+	killall mysqld
+	sleep 10s
 	mysql -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" && \
 		mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;" && \
 		mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" && \
