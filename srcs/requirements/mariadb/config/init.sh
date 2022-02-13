675e0ced9f05b4249d722dd69da94d7c46118f86
Grégory Artaud <gregory.rtd@gmail.com>
2022-02-13 23:08:44 +0100

    fix: access to db as root without pwd

--- srcs/requirements/mariadb/config/init.sh
+++ srcs/requirements/mariadb/config/init.sh
@@ -1,19 +1,31 @@
-service mysql start
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-	mysql_install_db
-	/usr/bin/mysqld_safe &
-	sleep 10s
-	echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" | mysql
-	killall mysqld
-	sleep 10s
-	mysql -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" && \
-		mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;" && \
-		mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" && \
-		mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;" && \
-		mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+	service mysql start
+
+	# Secure installation
+	mysql -e "UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PASSWORD}') WHERE User='root';"
+	mysql -e "DELETE FROM mysql.user WHERE User='';"
+	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	mysql -e "DROP DATABASE IF EXISTS test;"
+	mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+	mysql -e "FLUSH PRIVILEGES;"
+
+	# Prevent unauthorized access from wordpress
+	mysql -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;"
+
+	mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;"
+
+	# Apply changes
+	mysql -e "FLUSH PRIVILEGES;"
+
+	# Create database
+	mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 	mysql ${DB_NAME} < /wordpress.sql
+
+	# Set root password
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
+	
 	service mysql stop
 fi
-#tail -f
-#service mysql start
-mysqld
+mysqld_safe
